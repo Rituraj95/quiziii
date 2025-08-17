5c24a1dddaabec9aa5df38892a611ec1e6f40f45
Rituraj Deka <rituraj.deka@localwell.in>
2025-08-17 19:34:15 +0530

    added quiz2

--- src/components/quiz2quetions.js
+++ src/components/quiz2quetions.js
@@ -1,12 +1,106 @@
-export const quiz2= {
-  title: "Coming soon",
+export const quiz2 = {
+  title: "Internet Pdf 2 Quiz",
   questions: [
     {
-      question: "Coming Soon.....",
-      options: ["", "", " ", ""],
-      answer: 2
+      question: "What is the primary function of cookies in a web browser?",
+      options: [
+        "To store website passwords permanently",
+        "To provide quick access by storing browsing history at the client end",
+        "To block advertisements",
+        "To increase internet speed"
+      ],
+      answer: 1
     },
-   
-   
+    {
+      question: "Which symbol represents a hyperlink?",
+      options: [
+        "Arrow sign",
+        "Hand sign",
+        "Exclamation mark",
+        "Question mark"
+      ],
+      answer: 1
+    },
+    {
+      question: "What is the file extension for a Flash animation file?",
+      options: [
+        ".pdf",
+        ".swf",
+        ".psd",
+        ".html"
+      ],
+      answer: 1
+    },
+    {
+      question: "Which of the following is a markup language with user-defined tags?",
+      options: [
+        "HTML",
+        "XML",
+        "CSS",
+        "JavaScript"
+      ],
+      answer: 1
+    },
+    {
+      question: "What does DTP stand for?",
+      options: [
+        "Data Transfer Protocol",
+        "Desktop Publishing",
+        "Dynamic Text Processing",
+        "Document Type Page"
+      ],
+      answer: 1
+    },
+    {
+      question: "Which software is used to support animations on websites?",
+      options: [
+        "Adobe Photoshop",
+        "Adobe Flash Player",
+        "CorelDraw",
+        "Adobe Reader"
+      ],
+      answer: 1
+    },
+    {
+      question: "Who is known as the 'Father of the Internet'?",
+      options: [
+        "Tim Berners-Lee",
+        "Vinton Cerf",
+        "Mark Andressenn",
+        "Bill Gates"
+      ],
+      answer: 1
+    },
+    {
+      question: "Which of the following is a static webpage characteristic?",
+      options: [
+        "Automatic updates are possible",
+        "Made using HTML only",
+        "Requires server-side scripting",
+        "Dynamic content loading"
+      ],
+      answer: 1
+    },
+    {
+      question: "What is the shortcut to open a new incognito window in a browser?",
+      options: [
+        "Ctrl + T",
+        "Ctrl + Shift + N",
+        "Ctrl + H",
+        "Ctrl + Shift + B"
+      ],
+      answer: 1
+    },
+    {
+      question: "Which was the first successful web browser?",
+      options: [
+        "Mosaic",
+        "Netscape Navigator",
+        "Internet Explorer",
+        "Google Chrome"
+      ],
+      answer: 1
+    },
+    
   ]
 };
